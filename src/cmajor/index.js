26def16d9001231c077a7c80f8822747ae74dd61
Loowps <52254689+loowps@users.noreply.github.com>
2023-04-03 22:56:36 +0200

    Replace iframe with angular elements web component

--- src/cmajor/index.js
+++
@@ -1,8 +0,0 @@
-export default function createCustomPatchView(patchConnection) {
-  const iframe = document.createElement('iframe');
-  iframe.src = './index.html';
-  window['patchConnection'] = patchConnection;
-
-  iframe.setAttribute('style', 'width:100vw !important; height:100vh; border: none;');
-  return iframe;
-}
