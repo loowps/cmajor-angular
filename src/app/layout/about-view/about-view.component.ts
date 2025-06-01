fe23fa281005569158f3926b447306025a29f813
Loowps <52254689+loowps@users.noreply.github.com>
2025-06-01 16:02:17 +0200

    Update Angular to v20

--- src/app/layout/about-view/about-view.component.ts
+++ src/app/layout/about-view/about-view.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'cmaj-about',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './about-view.component.html',
   styleUrls: ['./about-view.component.scss'],
 })
